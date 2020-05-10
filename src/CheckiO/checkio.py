8281a0d823694f50cee7572c0834264c7576c278
Alexander Lyabah <a.lyabah@checkio.org>
2020-05-10 23:09:13 +0300

    move to tools, keymap and popup

--- src/CheckiO/checkio.py
+++ src/CheckiO/checkio.py
@@ -3,9 +3,26 @@ import sublime_plugin
 import subprocess
 import sys
 import shlex
+import json
 
+SYSTEM_BLOCK_START = '---SYSTEMBLOCKSTART---'
+SYSTEM_BLOCK_END = '---SYSTEMBLOCKEND---'
 
 class RunCommand(sublime_plugin.TextCommand):
+    def sysinfo__passed(self, data):
+        sublime.message_dialog('''
+!!! MISSION SOLVED!!!
+
+Link to solutions:
+{solutions}
+
+Link for sharing own solutions:
+{share}
+        '''.format(
+            solutions=data['solutions_link'],
+            share=data['add_link'],
+        ))
+
     def run(self, edit):
         sublime.set_timeout_async(self.run_release, 0)
 
@@ -30,12 +47,26 @@ class RunCommand(sublime_plugin.TextCommand):
         print('>>> {}'.format(exec_line))
         commands = shlex.split(exec_line)
         proc = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        is_capturing_sys = False
+        sysinfo = ''
         while proc.poll() is None:
             data = proc.stdout.readline().decode('utf-8')
-            print(data, end="")
+            if data.strip() == SYSTEM_BLOCK_START:
+                is_capturing_sys = True
+            elif data.strip() == SYSTEM_BLOCK_END:
+                is_capturing_sys = False
+            elif is_capturing_sys:
+                sysinfo += data
+            else:
+                print(data, end="")
         print(proc.stderr.read().decode('utf-8'))
         print('<<< Done')
 
+        if sysinfo:
+            sysinfo = json.loads(sysinfo)
+            getattr(self, 'sysinfo__' + sysinfo['info'])(sysinfo)
+
 class CheckCommand(RunCommand):
     def run_next(self, code, first_line):
-        self.exec_command(first_line + ' ' + self.view.file_name() + ' --check')
+        self.exec_command(first_line + ' ' + self.view.file_name() + ' --check --sysinfo')
